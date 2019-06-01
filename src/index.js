48e35f786a1a716d2f51d486d1d3e7ddcb202eb0
Samba <rocklee.no21@gmail.com>
2019-06-01 11:47:06 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './store/reducer';
 
